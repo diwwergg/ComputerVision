5d76d84805c8523ad3ee1697663243b4e54f3b8d
diwwergg <tawatchaibuajun@gmail.com>
2022-09-29 18:06:21 +0700

    week 4 Filter

--- Week4/Edge.py
+++ Week4/Edge.py
@@ -1,11 +1,17 @@
 import cv2
-
+import numpy as np
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 while True:
     _, frame = cap.read()
-    frame2 = frame[:frame.shape[0] // 2, frame.shape[1] // 2 + 20:, :]
-    cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Canny', cv2.Canny(frame2, 100, 200))
-    cv2.imshow('Canny', cv2.Canny(frame, 100, 200))
+    frame = frame[:frame.shape[0] // 2, frame.shape[1]//2 + 20:, :]
+    frame0 = frame.copy()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    temp = cv2.Canny(frame, 100, 200)
+    s = np.sum(temp[:temp.shape[0]//2, :])
+    print(s)
+    text = 'paper' if s > 0 else 'rock'
+    cv2.putText(frame0, text, (frame.shape[1] - 100, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, 1)
+    cv2.imshow('Canny', frame)
     cv2.waitKey(1)
